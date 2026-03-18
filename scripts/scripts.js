1365c9dd53ce3dd02f9cbc7638f765a25b0238f3
LuisCacho-py <luisg.cacho@outlook.com>
2026-03-18 13:39:14 -0600

    Agregue scripts.js

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+// Variables
+let variable1 = 10;
+let variable2 = 20;
+
+// Suma de variables
+let suma = variable1 + variable2;
+
+// Alerta
+alert("La suma de " + variable1 + " + " + variable2 + " = " + suma);
